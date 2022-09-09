a71e54e918771d55b4b714342092fd3dafe15fc6
marcos <marcos@li0001>
2022-09-09 15:58:06 -0300

    Initial commit.

--- src/main/java/com/trybe/agenda/AgendaMapa.java
+++ src/main/java/com/trybe/agenda/AgendaMapa.java
@@ -1,7 +1,34 @@
 package com.trybe.agenda;
 
+import java.util.HashMap;
+
+/** Classe AgendaMapa. */
 public class AgendaMapa {
-
-
+  HashMap<String, String> contatos = new HashMap<String, String>();
+  
+  void incluirContato(String nome, String telefone) {
+    contatos.put(nome, telefone);
+  }
+  
+  void excluirContato(String nome) {
+    contatos.remove(nome);
+  }
+  
+  boolean verificaContato(String nome) {
+    return contatos.containsKey(nome);
+  }
+  
+  int tamanhoAgenda() {
+    return contatos.size();
+  }
+  
+  String atualizarTelefone(String nome, String telefone) {
+    contatos.put(nome, telefone);
+    return contatos.get(nome);
+  }
+  
+  String obterTelefone(String nome) {
+    return contatos.get(nome);
+  }
 
 }
